b20c006a6e9475a84c51a7027af05dbe15f2ac14
Louiscata <lbcpdello@gmail.com>
2023-11-10 12:21:52 +0100

    10/11/2023

--- Practicas Redes/Practica3/clienteUDP.c
+++ Practicas Redes/Practica3/clienteUDP.c
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[])
 {
 	/*Nos aseguramos de que el número de parámetros que se le pasan al programa a través de la línea de comandos es correcto.
-	En el caso de nuestro emisor UDP, deberíamos tener:
+	En el caso de nuestro cliente UDP, deberíamos tener:
 		(1) argv[0] -> Nombre del ejecutable.
 		(2) argv[1] -> Nombre del archivo de origen.
 		(3) argv[2] -> Puerto propio.
@@ -22,28 +22,28 @@ int main(int argc, char *argv[])
 
 	if (argc < 5)
 	{
-		perror("Error. Introduzca los parametros adecuados en la linea de comandos\n");
+		perror("Error. Introduzca los Parametros Adecuados en la Linea de Comandos\n");
 		exit(EXIT_FAILURE);
 	}
 
-	// Abrimos los archivos de origen y destino
+	/*Gestión de los archivos origen y destino.*/
 	FILE *origen;
 	if ((origen = fopen(argv[1], "r")) == NULL)
 	{
-		perror("No se pudo abrir el archivo\n");
-		return EXIT_FAILURE;
+		perror("Error en la Apertura del Archivo Origen\n");
+		exit (EXIT_FAILURE);
 	}
 
 	char archivo_destino[strlen(argv[1])];
 	strcpy(archivo_destino, argv[1]);
-	for (int i = 0; argv[1][i] != '.'; i++)
+	for (int i = 0; i <= strlen(argv[1]); i++)
 		archivo_destino[i] = toupper(archivo_destino[i]);
 
 	FILE *destino;
 	if ((destino = fopen(archivo_destino, "w+")) == NULL)
 	{
-		perror("No se pudo abrir el archivo\n");
-		return EXIT_FAILURE;
+		perror("Error en la Apertura del Archivo Destino\n");
+		exit(EXIT_FAILURE);
 	}
 
 	/*Convertimos las cadenas de caracteres almacenadas en argv[2] y en argv[4] (número de los puertos) en enteros. Para ello, recurrimos a la función atoi()*/
@@ -53,11 +53,13 @@ int main(int argc, char *argv[])
 	/*Los puertos utilizados deben respetar la condición de ser mayores que IPPORT_USERRESERVED; es decir, no pueden estar reservados.*/
 	if (puertoPropio < IPPORT_USERRESERVED || puertoDestino < IPPORT_USERRESERVED)
 	{
-		perror("Error. Alguno de los puertos es incorrecto.\n");
+		perror("Error. Alguno de los Puertos es Incorrecto.\n");
 		exit(EXIT_FAILURE);
 	}
 
-	/*1. CREACIÓN DEL SOCKET DEL EMISOR HACIENDO USO DE LA FUNCIÓN socket()
+
+
+	/*1. CREACIÓN DEL SOCKET DEL CLIENTE HACIENDO USO DE LA FUNCIÓN socket()
 	- La función socket() crea un nuevo socket.
 	- Parámetros de Entrada:
 		(1) int domain: AF_INET (empleo de direcciones IPv4).
@@ -68,9 +70,11 @@ int main(int argc, char *argv[])
 	int propioSocket = socket(AF_INET, SOCK_DGRAM, 0);
 	if (propioSocket < 0)
 	{
-		perror("Error en la Creacion del Socket Emisor\n");
+		perror("Error en la Creacion del Socket Cliente\n");
 		exit(EXIT_FAILURE);
 	}
+
+
 
 	/*2. ASIGNACIÓN DE DIRECCIÓN Y PUERTO AL SOCKET A TRAVÉS DE LA FUNCIÓN bind()
 	- La función bind() es utilizada para asignar dirección y puerto a un socket.
@@ -83,7 +87,7 @@ int main(int argc, char *argv[])
 		(3) socklen_t length: Tamaño de la estructura.
 	- Parámetros de Salida: La función devuelve 0 en caso de éxito, y -1 en caso de error.*/
 
-	/*Estructura del emisor con IP y puerto propios (INADDR_ANY y el puerto elegido).*/
+	/*Estructura del cliente con IP y puerto propios (INADDR_ANY y el puerto elegido).*/
 	struct sockaddr_in propio_addr;
 	propio_addr.sin_family = AF_INET;
 	propio_addr.sin_port = htons(puertoPropio); // Puerto en orden de red.
@@ -96,6 +100,8 @@ int main(int argc, char *argv[])
 		perror("Error en la Asignacion de Puerto y Direccion\n");
 		exit(EXIT_FAILURE);
 	}
+
+
 
 	/*3. ENVÍO DEL MENSAJE UTILIZANDO LA FUNCIÓN sendto()
 	- La función sendto() hace posible el envío de datos sin conexión.
@@ -114,7 +120,7 @@ int main(int argc, char *argv[])
 	/*Almacenamiento de la IP remota (valor de argv[3]) en una variable.*/
 	char *ipDestino = argv[3];
 
-	/*Estructura del emisor con IP y puerto remotos.*/
+	/*Estructura del cliente con IP y puerto remotos.*/
 	struct sockaddr_in destino_addr;
 	destino_addr.sin_family = AF_INET;
 	destino_addr.sin_port = htons(puertoDestino); // Puerto en orden de red.
@@ -123,45 +129,52 @@ int main(int argc, char *argv[])
 	inet_pton se utiliza para convertir una dirección IP en formato legible en su formato binario correspondiente.*/
 	if (inet_pton(AF_INET, ipDestino, &(destino_addr.sin_addr)) <= 0)
 	{
-		perror("Problema al Configurar Direccion\n");
+		perror("Error en el Tratamiento de la IP\n");
 		exit(EXIT_FAILURE);
 	}
 
 	/*ENVÍO DE LOS DATOS LINEA A LINEA*/
 	char linea[MAX_LENGTH];
 	char buffer[MAX_LENGTH];
-	ssize_t bytes_enviados = 0;
+	
 	while (fgets(linea, sizeof(linea), origen) != NULL)
 	{
 		ssize_t bytesEnviados = sendto(propioSocket, linea, strlen(linea) + 1, 0, (struct sockaddr *)&destino_addr, sizeof(destino_addr));
 		if (bytesEnviados < 0)
 		{
-			perror("Error en el envio del Mensaje\n");
+			perror("Error en el Envio del Mensaje\n");
 			exit(EXIT_FAILURE);
 		}
-		else
-		{
-			/*Impresión del Número de Bytes Enviados.*/
-			printf("Se han enviado %ld bytes\n", bytesEnviados);
-		}
-
-		/*RECEPCIÓN DEL MENSAJE*/
+		
+		
+		/*4. RECEPCIÓN DEL MENSAJE UTILIZANDO LA FUNCIÓN recvfrom()
+		- La función recvfrom() hace posible la recepción de datos sin conexión.
+		- Parámetros de Entrada:
+			(1) int socket: Entero identificador del socket.
+			(2) void *buffer: Puntero donde se van a recibir los datos.
+			(3) size_t size: Número máximo de bytes a recibir.
+			(4) int flags: Opciones de recepción (por defecto, int flags = 0).
+			(5) struct sockaddr *addr: addr salida es un puntero a un struct sockaddr con la dirección de la procedencia del paquete.
+			(6) socklen_t *length_ptr: length_ptr es un puntero que es parámetro de entrada indicando el tamaño de la estructura addr, y de salida con el espacio real consumido.
+		- Parámetros de Salida: La función devuelve el número de bytes recibidos (éxito) o -1 (error).*/
+		
 		socklen_t destino_len = sizeof(destino_addr);
 		ssize_t bytesRecibidos = recvfrom(propioSocket, buffer, MAX_LENGTH, 0, (struct sockaddr *)&destino_addr, &destino_len);
-		if (bytesRecibidos <= 0)
+		
+		if (bytesRecibidos < 0)
 		{
-			perror("Error al Recibir el mensaje\n");
+			perror("Error al Recibir el Mensaje\n");
 			exit(EXIT_FAILURE);
 		}
-		/*Imprimimos el mensaje y lo escribimos en el archivo.*/
+		
+		/*Escribimos el mensaje en el archivo destino.*/
 		printf("Bytes recibidos: %ld\n", bytesRecibidos);
-
-		// printf("\nString salida: %s",buffer);
-
 		fputs(buffer, destino);
 	}
 
-	/*4. FUNCIÓN close()
+
+
+	/*5. FUNCIÓN close()
 	- La función close() cierra un socket.
 	- Parámetro de Entrada:
 		(1) int socket: Entero identificador del socket que queremos cerrar.
--- Practicas Redes/Practica3/servidorUDP.c
+++ Practicas Redes/Practica3/servidorUDP.c
@@ -13,27 +13,29 @@
 int main(int argc, char *argv[])
 {
 	/*Nos aseguramos de que el número de parámetros que se le pasan al programa a través de la línea de comandos es correcto.
-	En el caso de nuestro receptor UDP, deberíamos tener:
+	En el caso de nuestro servidor UDP, deberíamos tener:
 		(1) argv[0] -> Nombre del ejecutable.
 		(2) argv[1] -> Puerto.*/
 
 	if (argc < 2)
 	{
-		perror("Error. Introduzca los parametros adecuados en la linea de comandos\n");
+		perror("Error. Introduzca los Parametros Adecuados en la Linea de Comandos\n");
 		exit(EXIT_FAILURE);
 	}
 
 	/*Convertimos la cadena de caracteres almacenada en argv[1] (número del puerto) en un entero. Para ello, recurrimos a la función atoi()*/
 	int puerto = atoi(argv[1]);
 
-	/*El puerto utilizado debe respetar la condición de ser mayor que IPPORT_USERRESERVED; es decir, no pueden estar reservados.*/
+	/*El puerto utilizado debe respetar la condición de ser mayor que IPPORT_USERRESERVED; es decir, no puede estar reservado.*/
 	if (puerto < IPPORT_USERRESERVED)
 	{
-		perror("Error. Puerto incorrecto.\n");
+		perror("Error. Puerto Incorrecto\n");
 		exit(EXIT_FAILURE);
 	}
 
-	/*1. CREACIÓN DEL SOCKET DEL RECEPTOR HACIENDO USO DE LA FUNCIÓN socket()
+
+
+	/*1. CREACIÓN DEL SOCKET DEL SERVIDOR HACIENDO USO DE LA FUNCIÓN socket()
 	- La función socket() crea un nuevo socket.
 	- Parámetros de Entrada:
 		(1) int domain: AF_INET (empleo de direcciones IPv4).
@@ -44,9 +46,11 @@ int main(int argc, char *argv[])
 	int propioSocket = socket(AF_INET, SOCK_DGRAM, 0);
 	if (propioSocket < 0)
 	{
-		perror("Error en la Creacion del Socket Emisor\n");
+		perror("Error en la Creacion del Socket Servidor\n");
 		exit(EXIT_FAILURE);
 	}
+
+
 
 	/*2. ASIGNACIÓN DE DIRECCIÓN Y PUERTO AL SOCKET A TRAVÉS DE LA FUNCIÓN bind()
 	- La función bind() es utilizada para asignar dirección y puerto a un socket.
@@ -54,15 +58,15 @@ int main(int argc, char *argv[])
 		(1) int socket: Entero identificador del socket.
 		(2) struct sockaddr *addr: addr es un puntero a un struct sockaddr con la dirección que se le quiere asignar al socket.
 			(2.1) sin_family: AF_INET
-			(2.2) sin_port: Puerto propio del receptor (el puerto elegido).
+			(2.2) sin_port: Puerto propio (el puerto elegido).
 			(2.3) sin_addr.s_addr: INADDR_ANY para poder aceptar peticiones que lleguen por cualquier interfaz.
 		(3) socklen_t length: Tamaño de la estructura.
 	- Parámetros de Salida: La función devuelve 0 en caso de éxito, y -1 en caso de error.*/
 
-	/*Estructura del emisor con IP y puerto propios (INADDR_ANY y el puerto elegido).*/
+	/*Estructura del servidor con IP y puerto propios (INADDR_ANY y el puerto elegido).*/
 	struct sockaddr_in propio_addr;
 	propio_addr.sin_family = AF_INET;
-	propio_addr.sin_port = htons(puerto); // Puerto en orden de red.
+	propio_addr.sin_port = htons(puerto);  // Puerto en orden de red.
 
 	/*La macro INADDR_ANY está en orden de host, por lo que tenemos que cambiarla a orden de red.*/
 	propio_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -72,6 +76,8 @@ int main(int argc, char *argv[])
 		perror("Error en la Asignacion de Puerto y Direccion\n");
 		exit(EXIT_FAILURE);
 	}
+
+
 
 	/*3. RECEPCIÓN DEL MENSAJE UTILIZANDO LA FUNCIÓN recvfrom()
 	- La función recvfrom() hace posible la recepción de datos sin conexión.
@@ -84,17 +90,17 @@ int main(int argc, char *argv[])
 		(6) socklen_t *length_ptr: length_ptr es un puntero que es parámetro de entrada indicando el tamaño de la estructura addr, y de salida con el espacio real consumido.
 	- Parámetros de Salida: La función devuelve el número de bytes recibidos (éxito) o -1 (error).*/
 
+	/*Estructura del servidor con IP y puerto remotos. No es necesario inicializarla.*/
+	struct sockaddr_in remoto_addr;
+	socklen_t remoto_len = sizeof(remoto_addr);
+
+	char buffer[TAM];
+
 	while (1)
 	{
-		/*Estructura del emisor con IP y puerto remotos. No es necesario inicializarla.*/
-		struct sockaddr_in remoto_addr;
-		socklen_t remoto_len = sizeof(remoto_addr);
-
-		char buffer[TAM];
-
 		/*RECEPCIÓN DEL MENSAJE*/
 		ssize_t bytesRecibidos = recvfrom(propioSocket, buffer, TAM, 0, (struct sockaddr *)&remoto_addr, &remoto_len);
-		if (bytesRecibidos <= 0)
+		if (bytesRecibidos < 0)
 		{
 			perror("Error al Recibir el mensaje\n");
 			exit(EXIT_FAILURE);
@@ -107,35 +113,49 @@ int main(int argc, char *argv[])
 		inet_ntop toma una dirección IP en su formato binario y la convierte en una cadena legible por humanos.*/
 		if ((inet_ntop(AF_INET, &(remoto_addr.sin_addr), remotoIP, INET_ADDRSTRLEN)) == NULL)
 		{
-			perror("Error en el tratamiento de la IP\n");
+			perror("Error en el Tratamiento de la IP\n");
 			exit(EXIT_FAILURE);
 		}
 
-		/*Imprimimos: La IP y el puerto del que envía, el número de bytes recibidos y el mensaje leído.*/
+		/*Imprimimos: La IP y el puerto del que envía.*/
 		printf("IP [%s] y puerto [%d]\n", remotoIP, ntohs(remoto_addr.sin_port));
-		printf("Mensaje recibido: [%s]\n", buffer);
-		printf("Se han recibido %ld bytes\n", bytesRecibidos);
 
+
+		/*Conversión del mensaje a mayúsculas a través de la función toupper()*/
 		char mensaje[bytesRecibidos];
 
-		// Transformamos el mensaje a mayúsculas
-		printf("Convirtinedo mensaje...\n");
 		for(int i = 0; i < bytesRecibidos + 1; i++)
 		{
 			mensaje[i] = toupper(buffer[i]);
 		}
 
-		//Enviamos el mensaje al cliente
-		ssize_t bytesEnviados = sendto(propioSocket, mensaje, strlen(mensaje) + 1, 0, (struct sockaddr *)&remoto_addr, sizeof(remoto_addr));
+		
+		
+		/*4. ENVÍO DEL MENSAJE UTILIZANDO LA FUNCIÓN sendto()
+		- La función sendto() hace posible el envío de datos sin conexión.
+		- Parámetros de Entrada:
+			(1) int socket: Entero identificador del socket.
+			(2) void *buffer: Puntero a los datos a enviar.
+			(3) size_t size: Número de bytes a enviar.
+			(4) int flags: Opciones de envío (por defecto, int flags = 0)
+			(5) struct sockaddr *addr: addr es un puntero a un struct sockaddr con la dirección a la que se quieren enviar los datos.
+				(5.1) sin_family
+				(5.2) sin_port: Puerto remoto.
+				(5.3) sin_addr.s_addr
+			(6) socklen_t length: Tamaño de la estructura addr.
+		- Parámetros de Salida: Número de bytes transmitidos aunque sea con errores (éxito), o -1 (error).*/
+		
+		ssize_t bytesEnviados = sendto(propioSocket, mensaje, sizeof(mensaje), 0, (struct sockaddr *)&remoto_addr, sizeof(remoto_addr));
 		if (bytesEnviados < 0)
 		{
 			perror("Error en el Envio del Mensaje\n");
 			exit(EXIT_FAILURE);
 		}
-		printf("Se han enviado %ld bytes\n\n", bytesEnviados);
 	}
 
-	/*4. FUNCIÓN close()
+
+
+	/*5. FUNCIÓN close()
 	- La función close() cierra un socket.
 	- Parámetro de Entrada: Entero identificador del socket que queremos cerrar.
 	- Parámetro de Salida: La función close() devuelve 0 en caso de éxito, y un valor distinto del nulo en caso de error.*/
